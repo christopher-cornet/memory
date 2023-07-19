ec9096f957bb124885a78ea90f6e2a4bd41b0c68
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-07-19 16:00:00 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+import React from 'react';
+import Verso from './components/verso/verso';
+// import verso from './img/verso.jpg'; // Face Down Card
+import dialga from './img/dialga.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="memory-game">
+        <div class="left-options">
+          <p>Test</p>
+        </div>
+        <div class="cards-grid">
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          <Verso/>
+          
+          {/* <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={dialga}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/>
+          <img src={verso}/> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/verso/verso.js
@@ -0,0 +1,10 @@
+import React from "react";
+import darksidecard from "../../img/verso.jpg";
+
+export default function Verso() {
+    return (
+      <img
+        src={darksidecard}
+      />
+    )
+  }
